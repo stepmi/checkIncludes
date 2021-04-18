3035514ec2cd672878d7d1b4446a0d104c03e5e9
Michael Stephan <stepmi@gmx.de>
2021-04-18 13:11:13 +0200

    splitString

--- src/inc/tools/strings.h
+++ src/inc/tools/strings.h
@@ -12,7 +12,7 @@ namespace tools
 	{
 		std::string getQuoted(const std::string &a);
 
-		
+
 		template<typename T> bool replace(T &a_sMain, const T &a_sFind, const T &a_sReplace, const bool bReplaceAll)
 		{
 			if (a_sFind.size() == 0)
@@ -32,7 +32,7 @@ namespace tools
 			} while (bReplaceAll && pos != T::npos);
 
 			return bFound;
-		}				
+		}
 
 		template<typename T> std::string itos(const T a_iVal)
 		{
@@ -41,13 +41,34 @@ namespace tools
 			return os.str();
 		}
 
+		template<typename T> std::vector<T> splitString(const T& a_sSource, const T& a_sSeparator)
+		{
+			std::vector<T> result;
+			typename T::size_type iPosStart = 0;
+			while (true)
+			{
+				const typename T::size_type iPosSeperator = a_sSource.find(a_sSeparator, iPosStart);
+				if (T::npos != iPosSeperator)
+				{
+					result.emplace_back(a_sSource.substr(iPosStart, iPosSeperator - iPosStart));
+					iPosStart = iPosSeperator + a_sSeparator.length();
+				}
+				else
+				{
+					result.emplace_back(a_sSource.substr(iPosStart));
+					break;
+				}
+			}
+			return result;
+		}
+
 		std::string secondsToHours(const long long a_iSeconds);
 
 #ifdef _WIN32
 		std::wstring atow(const std::string& a_Src);
 		std::string wtoa(const std::wstring& a_Src);
 #endif
-		
+
 		bool compareCaseInsensitive(const char a, const char b);
 		bool compareCaseInsensitive(const std::string &a, const std::string &b);
 		bool compareCaseInsensitive(const platform::string &a, const platform::string &b);
